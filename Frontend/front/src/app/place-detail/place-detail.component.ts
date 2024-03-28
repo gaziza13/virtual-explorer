55e042b98a2a31b46d52c075b742508523ca527f
gaziza13 <gazeka.04@mail.ru>
2024-03-28 21:26:10 +0500

    commit

---
+++ Frontend/front/src/app/place-detail/place-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './place-detail.component.html',
+  styleUrl: './place-detail.component.css'
+})
+export class PlaceDetailComponent {
+
+}
